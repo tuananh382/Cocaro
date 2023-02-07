a9699e5786827ced1b4b23201aeabd878911cbb5
tuantu123456 <122219423+tuantu123456@users.noreply.github.com>
2023-02-08 01:17:16 +0700

    done

--- sever/index.js
+++ sever/index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 const http = require("http");
+const { io } = require('socket.io-client');
 var app = express();
 const server = http.createServer(app);
 
@@ -8,30 +9,81 @@ const socketIo = require("socket.io")(server, {
         origin: "*",
     }
   });
-
-
+const roomplayer = [];
+var checkroom = 1;
+var pos;
 socketIo.on("connection", (socket) => {
-  console.log("New client connected" + socket.id);
-  
   socket.emit("getId", socket.id);
-
-  socket.on("sendIdClient", ids => {
-    socketIo.emit("sendIdServer", ids );
+  socket.on("joinroom", function(roomname) {
+    for(let i=0; i < roomplayer.length ;i=i+3) {
+      if(roomname[0] == roomplayer[i])
+        {
+          checkroom = 0;
+          pos = i
+        }
+      else
+        checkroom = 1
+    }
+    //Phong khong ton tai
+    if(checkroom == 1)
+      {
+        //Tao phong
+        roomplayer.push(roomname[0])       
+        //Tham gia phòng
+        socket.join(roomname[0]);
+        roomplayer.push(roomname[1])
+        roomplayer.push(0)
+        //Trả lại thông báo 
+        socketIo.to(roomname[0]).emit("notification", "Create Match Successful")
+        socket.on("sendIdClient", ids => {
+          socketIo.to(roomname[0]).emit("sendIdServer", ids );
+        })
+        socket.on("sendWinClient", win => {
+          socketIo.to(roomname[0]).emit("sendWinSever", win)
+        })
+        socket.on("sendDataClient", function(data) {
+          socketIo.to(roomname[0]).emit("sendDataServer", { data });
+        })
+      }
+      // Phong ton tai
+      else
+       {
+        if(roomplayer[pos+2] == 0)
+        {     
+            //Tham gia phòng
+            socket.join(roomname[0]);
+            roomplayer[pos + 2] = roomname[1]
+            //Trả lại thông báo 
+            socketIo.to(roomname[0]).emit("notification", roomname[1] + " was joined " + roomname[0]);
+            //Gui ten
+            const name = [roomplayer[pos+1],roomplayer[pos+2]]
+            socketIo.to(roomname[0]).emit("yourname", name);
+            socket.on("sendIdClient", ids => {
+              socketIo.to(roomname[0]).emit("sendIdServer", ids );
+            })
+            socket.on("sendWinClient", win => {
+              socketIo.to(roomname[0]).emit("sendWinSever", win)
+            })
+            socket.on("sendDataClient", function(data) {
+              socketIo.to(roomname[0]).emit("sendDataServer", { data });
+            })
+        }
+        else
+          socketIo.emit("full", "Phong Day")
+       }    
+  //Go out
+  socket.on("goout", data => {
+    socketIo.to(roomname[0]).emit("sendgoout", data)
   })
-
-  socket.on("sendWinClient", win => {
-    socketIo.emit("sendWinSever", win)
+  //Chat
+  socket.on("send", data => {
+    socketIo.to(roomname[0]).emit("sendtext", data)
   })
-
-  socket.on("sendDataClient", function(data) {
-    socketIo.emit("sendDataServer", { data });
-  })
-
-  socket.on("disconnect", () => {
+    });
+})
+socketIo.on("disconnect", () => {
     console.log("Client disconnected");
-  });
-});
-
+  })
 server.listen(3000, () => {
     console.log('Server đang chay tren cong 3000');
-});
+})
